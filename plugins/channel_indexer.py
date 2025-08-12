301b48a892ba61064f9fe291fb194da58a552da0
Autofilterbot1234 <168348712+Autofilterbot1234@users.noreply.github.com>
2025-08-12 21:19:39 +0600

    Update channel_indexer.py

--- plugins/channel_indexer.py
+++ plugins/channel_indexer.py
@@ -5,7 +5,7 @@ from database.ia_filterdb import save_file
 # শুধুমাত্র ডকুমেন্ট, ভিডিও এবং অডিও ফাইল ফিল্টার করা হচ্ছে
 media_filter = filters.document | filters.video | filters.audio
 
-@Client.on_message(filters.chat(CHANNELS) & media_filter & ~filters.edited)
+@Client.on_message(filters.chat(CHANNELS) & media_filter & ~filters.edited_message)
 async def channel_media_handler(client, message):
     """চ্যানেল থেকে আসা মিডিয়া ফাইল হ্যান্ডেল এবং সেভ করে"""
     media = message.document or message.video or message.audio
